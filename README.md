89912ec01e6b3679b4e2fb104b64e5cee2cfc852
RaufunNazin <raufun.nazin13@gmail.com>
2025-05-04 23:56:00 +0600

    Initialization

---
+++ README.md
@@ -0,0 +1,10 @@
+# FastAPI Starter Code
+
+A quick FastAPI starter code using minimal packages.
+
+## Features
+
+* Login (POST /login)/ Register (POST /users) functionality
+* Token based authentication (JWT)
+* Basic user table creation using SQLAlchemy
+* User information based on token (GET /me) and all users (GET /users)
---
+++ app/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from . import models
+from .database import engine
+from .routers import user
+from dotenv import load_dotenv
+
+load_dotenv()
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+origins = [
+    "http://localhost:5173",
+    "https://localhost:3000",
+    "http://localhost:3000",
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:8000",
+    "https://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def test_api():
+    return {"message": "Don't worry. API is working just fine."}
+
+app.include_router(user.router)
