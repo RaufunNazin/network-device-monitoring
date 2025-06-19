638784098320670074ffd98919502897569784c0
RaufunNazin <raufun.nazin13@gmail.com>
2025-06-19 15:26:49 +0600

    add snmp output formatter

--- app/utils.py
+++ app/utils.py
@@ -4,3 +4,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password) :
     return pwd_context.verify(plain_password, hashed_password)
+
+def format_mac(hex_mac):
+    clean_mac = hex_mac.replace(" ", "")
+    formatted_mac = ":".join([clean_mac[i:i+2] for i in range(0, len(clean_mac), 2)])
+    return formatted_mac
