1990cedd47befdac6145f6ff280153e7071c7d05
RaufunNazin <raufun.nazin13@gmail.com>
2025-06-19 16:45:56 +0600

    add raw value parser

---
+++ app/parsers/raw_value_parser.py
@@ -0,0 +1,45 @@
+from ..enums import OCTETSTRING, HEX_STRING, OID, OID_SHORT, GAUGE32, INTEGER, STRING, COUNTER32, COUNTER64, TIMETICKS, IPADDRESS, NULL
+
+def format_raw_values(value, value_type):
+  """
+  Format the value based on its type
+  Args:
+      value: The value to format.
+      value_type (str): The type of the value (e.g., OCTETSTRING, INTEGER).
+  Returns:
+      str: The formatted value as a string.
+  """
+  if value_type == OCTETSTRING:
+    decoded_value = value.prettyPrint()
+    if not decoded_value:  
+      return '""'
+    if not decoded_value.startswith("0x") and all(32 <= ord(char) <= 126 for char in decoded_value):
+      return f'{STRING}: "{decoded_value}"'
+    else:
+      hex_value = " ".join([f"{byte:02X}" for byte in value.asNumbers()])
+      return f'{HEX_STRING}: {hex_value}'
+  elif value_type == OID:
+    return f"{OID_SHORT}: {value.prettyPrint()}"
+  elif value_type == GAUGE32:
+    return f"{GAUGE32}: {value}"
+  elif value_type == COUNTER32:
+    return f"{COUNTER32}: {value}"
+  elif value_type == COUNTER64:
+    return f"{COUNTER64}: {value}"
+  elif value_type == TIMETICKS:
+    raw_ticks = int(value)
+    days = raw_ticks // (24 * 60 * 60 * 100)
+    hours = (raw_ticks // (60 * 60 * 100)) % 24
+    minutes = (raw_ticks // (60 * 100)) % 60
+    seconds = (raw_ticks // 100) % 60
+    milliseconds = raw_ticks % 100
+    human_readable = f"{days} days, {hours}:{minutes:02}:{seconds:02}.{milliseconds:02}"
+    return f"{TIMETICKS}: ({raw_ticks}) {human_readable}"
+  elif value_type == IPADDRESS:
+    return f"{IPADDRESS}: {value.prettyPrint()}"
+  elif value_type == INTEGER:
+    return f"{INTEGER}: {value}"
+  elif value_type == NULL:
+    return '""'
+  else:
+    return f"{value_type}: {value.prettyPrint()}"
--- app/utils.py
+++ app/utils.py
@@ -2,10 +2,31 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_password(plain_password, hashed_password) :
+
+def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
 
 def format_mac(hex_mac):
     clean_mac = hex_mac.replace(" ", "")
-    formatted_mac = ":".join([clean_mac[i:i+2] for i in range(0, len(clean_mac), 2)])
+    formatted_mac = ":".join(
+        [clean_mac[i : i + 2] for i in range(0, len(clean_mac), 2)]
+    )
     return formatted_mac
+
+
+def resolve_oid(oid, mib_view):
+    try:
+        oid_obj = ObjectIdentity(oid)
+        oid_obj.resolve_with_mib(mib_view)
+        return oid_obj.prettyPrint()
+    except Exception:
+        try:
+            mib_node = mib_view.get_node_name(oid)
+            module_name = mib_node[0]
+            obj_name = mib_node[1]
+            indices = list(oid[len(mib_view.get_node_oid(mib_node)) :])
+            index_str = "." + ".".join(map(str, indices)) if indices else ""
+            return f"{module_name}::{obj_name}{index_str}"
+        except Exception:
+            return oid.prettyPrint()
