155d6e7465535b7cc8210125c9582cbff465a6c4
RaufunNazin <raufun.nazin13@gmail.com>
2025-07-02 14:55:24 +0600

    update and format oid_name_dict

--- app/dictionaries/oid_name_dict.py
+++ app/dictionaries/oid_name_dict.py
@@ -1,99 +1,120 @@
-from ..enums import MAC, OPERATION_STATUS, ADMIN_STATUS, DISTANCE, UP_SINCE, VENDOR, MODEL, SERIAL_NO, POWER, DESC, PON, ONU, CDATA_EPON, CDATA_GPON, VSOL_EPON, VSOL_GPON, BDCOM_EPON, BDCOM_GPON
+from enums import (
+    MAC,
+    OPERATION_STATUS,
+    ADMIN_STATUS,
+    DISTANCE,
+    UP_SINCE,
+    VENDOR,
+    MODEL,
+    SERIAL_NO,
+    POWER,
+    DESC,
+    PON,
+    ONU,
+    CDATA_EPON,
+    CDATA_GPON,
+    VSOL_EPON,
+    VSOL_GPON,
+    BDCOM_EPON,
+    BDCOM_GPON,
+)
 
 oid_name_dictionary = {
-  MAC: {
-    CDATA_EPON: 'onuMacAddress',
-    CDATA_GPON: 'onuMacAddress',
-    VSOL_EPON: 'onuMacAddr',
-    VSOL_GPON: '1.3.6.1.4.1.37950.1.1.5.10.3.2.1.3',
-    BDCOM_EPON: 'onuMacAddr',
-    BDCOM_GPON: ''
-  },
-  SERIAL_NO: {
-    CDATA_EPON: 'onuSn',
-    CDATA_GPON: 'onuSn',
-    VSOL_EPON: 'onuID',
-    VSOL_GPON: 'gOnuDetailInfoSn',
-    BDCOM_EPON: 'onuID',
-    BDCOM_GPON: ''
-  },
-  OPERATION_STATUS: {
-    CDATA_EPON: 'onuOperationStatus',
-    CDATA_GPON: 'onuOperationStatus',
-    VSOL_EPON: 'onuPortBasicLinkStatus',
-    VSOL_GPON: 'gOnuDetailInfoOpSta',
-    BDCOM_EPON: '',
-    BDCOM_GPON: ''
-  },
-  ADMIN_STATUS: {
-    CDATA_EPON: 'onuAdminStatus',
-    CDATA_GPON: 'onuAdminStatus',
-    VSOL_EPON: 'onuPortBasicAdminStatus',
-    VSOL_GPON: 'gOnuStaInfoAdminSta',
-    BDCOM_EPON: '',
-    BDCOM_GPON: ''
-  },
-  DISTANCE: {
-    CDATA_EPON: 'onuTestDistance',
-    CDATA_GPON: 'onuTestDistance',
-    VSOL_EPON: 'onuAuthInfoDistance2',
-    VSOL_GPON: 'gOnuRttDistance',
-    BDCOM_EPON: 'onuDistance',
-    BDCOM_GPON: ''
-  },
-  UP_SINCE: {
-    CDATA_EPON: 'onuTimeSinceLastRegister',
-    CDATA_GPON: 'onuTimeSinceLastRegister',
-    VSOL_EPON: 'onuAuthInfoLastRegTime',
-    VSOL_GPON: 'gOnuDetailInfoSysUpTime',
-    BDCOM_EPON: 'onuAliveTime',
-    BDCOM_GPON: ''
-  },
-  VENDOR: {
-    CDATA_EPON: 'onuVendorId',
-    CDATA_GPON: 'onuVendorID',
-    VSOL_EPON: 'onuVendorId',
-    VSOL_GPON: 'gOnuDetailInfoVendorId',
-    BDCOM_EPON: 'onuVendorID',
-    BDCOM_GPON: ''
-  },
-  MODEL: {
-    CDATA_EPON: 'onuModelId',
-    CDATA_GPON: 'onuModelId',
-    VSOL_EPON: 'onuModel',
-    VSOL_GPON: 'gOnuModel',
-    BDCOM_EPON: '',
-    BDCOM_GPON: ''
-  },
-  POWER: {
-    CDATA_EPON: 'onuReceivedOpticalPower',
-    CDATA_GPON: 'onuReceivedOpticalPower',
-    VSOL_EPON: 'onuRecievepower',
-    VSOL_GPON: 'gOnuOpticalInfoRxPwr',
-    BDCOM_EPON: 'opModuleRxPower',
-    BDCOM_GPON: ''
-  },
-  DESC: {
-    CDATA_EPON: '',
-    CDATA_GPON: '',
-    VSOL_GPON: 'gOnuDetailInfoOnuDesc',
-    BDCOM_EPON: '',
-    BDCOM_GPON: ''
-  },
-  PON: {
-    CDATA_EPON: '',
-    CDATA_GPON: '',
-    VSOL_EPON: '',
-    VSOL_GPON: 'gOnuRttPon',
-    BDCOM_EPON: '',
-    BDCOM_GPON: ''
-  },
-  ONU: {
-    CDATA_EPON: '',
-    CDATA_GPON: '',
-    VSOL_EPON: '',
-    VSOL_GPON: 'gOnuRttOnu',
-    BDCOM_EPON: '',
-    BDCOM_GPON: ''
-  }
+    MAC: {
+        CDATA_EPON: "onuMacAddress",
+        CDATA_GPON: "onuAuthenSN",
+        VSOL_EPON: "onuMacAddr2",
+        VSOL_GPON: "macFlappingVlanId",
+        BDCOM_EPON: "onuMacAddr",
+        BDCOM_GPON: "",
+    },
+    SERIAL_NO: {
+        CDATA_EPON: "onuSn",
+        CDATA_GPON: "onuSerialNum",
+        VSOL_EPON: "onuID",
+        VSOL_GPON: "gOnuDetailInfoSn",
+        BDCOM_EPON: "onuID",
+        BDCOM_GPON: "onuSerialNum",
+    },
+    OPERATION_STATUS: {
+        CDATA_EPON: "onuOperationStatus",
+        CDATA_GPON: "onuOperationStatus",
+        VSOL_EPON: "lineStatus2",
+        VSOL_GPON: "gOnuDetailInfoOpSta",
+        BDCOM_EPON: "ifOperStatus",
+        BDCOM_GPON: "onuOperationalState",
+    },
+    ADMIN_STATUS: {
+        CDATA_EPON: "onuAdminStatus",
+        CDATA_GPON: "onuAdminStatus",
+        VSOL_EPON: "",
+        VSOL_GPON: "gOnuStaInfoAdminSta",
+        BDCOM_EPON: "ifAdminStatus",
+        BDCOM_GPON: "ifAdminStatus",
+    },
+    DISTANCE: {
+        CDATA_EPON: "onuTestDistance",
+        CDATA_GPON: "onuTestDistance",
+        VSOL_EPON: "onuAuthInfoDistance2",
+        VSOL_GPON: "gOnuRttDistance",
+        BDCOM_EPON: "onuDistance",
+        BDCOM_GPON: "onuDistance",
+    },
+    UP_SINCE: {
+        CDATA_EPON: "onuTimeSinceLastRegister",
+        CDATA_GPON: "onuTimeSinceLastRegister",
+        VSOL_EPON: "onuAuthInfoLastRegTime",
+        VSOL_GPON: "gOnuDetailInfoSysUpTime",
+        BDCOM_EPON: "onuAliveTime",
+        BDCOM_GPON: "onuSysUpTime",
+    },
+    VENDOR: {
+        CDATA_EPON: "onuVendorId",
+        CDATA_GPON: "onuVendorID",
+        VSOL_EPON: "onuVendorId",
+        VSOL_GPON: "gOnuDetailInfoVendorId",
+        BDCOM_EPON: "onuVendorID",
+        BDCOM_GPON: "onuVendorID",
+    },
+    MODEL: {
+        CDATA_EPON: "onuModelId",
+        CDATA_GPON: "onuEquipmentID",
+        VSOL_EPON: "onuModel",
+        VSOL_GPON: "gOnuModel",
+        BDCOM_EPON: "onuModuleID",
+        BDCOM_GPON: "onuEquipmentID",
+    },
+    POWER: {
+        CDATA_EPON: "onuReceivedOpticalPower",
+        CDATA_GPON: "onuReceivedOpticalPower",
+        VSOL_EPON: "rxPower",
+        VSOL_GPON: "gOnuOpticalInfoRxPwr",
+        BDCOM_EPON: "opModuleRxPower",
+        BDCOM_GPON: "gponOnuOpticalPowerRxPower",
+    },
+    DESC: {
+        CDATA_EPON: "",
+        CDATA_GPON: "",
+        # VSOL_EPON: 'onuOpmPowerName',
+        VSOL_EPON: "",
+        VSOL_GPON: "gOnuDetailInfoOnuDesc",
+        BDCOM_EPON: "ifName",
+        BDCOM_GPON: "ifDescr",
+    },
+    PON: {
+        CDATA_EPON: "",
+        CDATA_GPON: "",
+        VSOL_EPON: "authInfoPonNo2",
+        VSOL_GPON: "gOnuRttPon",
+        BDCOM_EPON: "",
+        BDCOM_GPON: "",
+    },
+    ONU: {
+        CDATA_EPON: "",
+        CDATA_GPON: "",
+        VSOL_EPON: "authInfoOnuNo2",
+        VSOL_GPON: "gOnuRttOnu",
+        BDCOM_EPON: "",
+        BDCOM_GPON: "",
+    },
 }
