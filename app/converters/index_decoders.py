ba991df95a788a6c9840a3feb14d3ed8d450586c
RaufunNazin <raufun.nazin13@gmail.com>
2025-07-02 15:29:29 +0600

    update index_decoder

--- app/converters/index_decoders.py
+++ app/converters/index_decoders.py
@@ -1,27 +1,35 @@
-from ..enums import SLOT_ID, CARD_ID, PON_ID, ONU_ID
+from ..enums import CDATA_EPON, CDATA_GPON, SLOT_ID, CARD_ID, PON_ID, ONU_ID
 
-def decode_cdata_epon(device_id):
-  slot = (device_id >> 24) & 0xFF
-  card = (device_id >> 16) & 0xFF
-  pon = (device_id >> 8) & 0xFF
-  onu = device_id & 0xFF
 
-  return {
-    SLOT_ID: slot,
-    CARD_ID: card,
-    PON_ID: (pon // 16) + 1,
-    ONU_ID: onu,
-}
-    
-def decode_cdata_gpon(device_id):
-  slot = (device_id >> 24) & 0xFF
-  card = (device_id >> 16) & 0xFF
-  pon = (device_id >> 8) & 0xFF
-  onu = device_id & 0xFF
+def _index_decoder(full_index, brand):
+    if brand == CDATA_EPON:
+        slot = (full_index >> 24) & 0xFF
+        card = (full_index >> 16) & 0xFF
+        pon = (full_index >> 8) & 0xFF
+        onu = full_index & 0xFF
 
-  return {
-    SLOT_ID: slot - 1 if slot > 0 else 0,
-    CARD_ID: card,
-    PON_ID: pon - 6,
-    ONU_ID: onu,
-}
+        return {
+            SLOT_ID: slot,
+            CARD_ID: card,
+            PON_ID: (pon // 16) + 1,
+            ONU_ID: onu,
+        }
+    elif brand == CDATA_GPON:
+        slot = (full_index >> 24) & 0xFF
+        card = (full_index >> 16) & 0xFF
+        pon = (full_index >> 8) & 0xFF
+        onu = full_index & 0xFF
+
+        return {
+            SLOT_ID: slot - 1 if slot > 0 else 0,
+            CARD_ID: card,
+            PON_ID: pon - 6,
+            ONU_ID: onu,
+        }
+
+
+if __name__ == "__main__":
+    # Example usage
+    device_id = 16780858  # Example device ID
+    print(_index_decoder(device_id, "CDATA-GPON"))
+    # print("CDATA GPON Decoded:", decode_cdata_gpon(device_id))
