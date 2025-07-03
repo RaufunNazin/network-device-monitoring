81941a5667c74647f64b8cee1eec95a7d69a8a17
RaufunNazin <raufun.nazin13@gmail.com>
2025-07-03 13:13:08 +0600

    create API template

--- app/routers/snmp.py
+++ app/routers/snmp.py
@@ -1,4 +1,46 @@
-import asyncio
-from ..enums import MAC, OPERATION_STATUS, ADMIN_STATUS, DISTANCE, UP_SINCE, VENDOR, MODEL, SERIAL_NO, POWER, DESC, CDATA_EPON, CDATA_GPON, VSOL_EPON, VSOL_GPON
-import argparse
-from ..utils import get_olt_information
+from ..enums import (
+    MAC,
+    OPERATION_STATUS,
+    ADMIN_STATUS,
+    DISTANCE,
+    UP_SINCE,
+    VENDOR,
+    MODEL,
+    SERIAL_NO,
+    POWER,
+    DESC,
+    CDATA_EPON,
+    CDATA_GPON,
+    VSOL_EPON,
+    VSOL_GPON,
+)
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/snmp",
+    tags=["SNMP"],
+)
+
+
+@router.get("/onu-ports")
+async def get_onu_ports():
+    """
+    Endpoint to retrieve ONU ports information.
+    """
+    return {"message": "This endpoint will return ONU ports information."}
+
+
+@router.get("/customer-mac")
+async def get_customer_mac():
+    """
+    Endpoint to retrieve customer MAC addresses.
+    """
+    return {"message": "This endpoint will return customer MAC addresses."}
+
+
+@router.get("/web-scraping")
+async def web_scraping():
+    """
+    Endpoint to perform web scraping.
+    """
+    return {"message": "This endpoint will perform web scraping."}
